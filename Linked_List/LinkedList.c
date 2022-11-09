0c8d41b9d17cbed1c349c71697e3e60c912b8515
darkrainbowsprinkles <113314204+ChoiBeomgyuItBoy@users.noreply.github.com>
2022-11-08 22:22:15 -0600

    Update LinkedList.c

--- Linked_List/LinkedList.c
+++ Linked_List/LinkedList.c
@@ -1,117 +1,226 @@
 //  ----------- Single Linked List implementation in C -----------
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Node
 {
-    int value;  
-    struct Node *next;
+  int data;
+  struct Node *next;
 } Node;
 
-Node *top; // Reference to the top of the List
-Node *end; // Reference to the end of the List
+Node *top;
+Node *end;
 
-Node *createNewNode(int value)
+Node *CreateNewNode(int data)
 {
-    Node *newNode = malloc(sizeof(malloc));
+  Node *newNode = malloc(sizeof(Node));
 
-    newNode -> value = value;
-    newNode -> next = NULL;
+  newNode -> data = data;
+  newNode -> next = NULL;
 
-    return newNode;
+  return newNode;
 }
 
-void add(int value) // Adds an element to the end of the List
+void AddToEnd(int data)
 {
-    Node *current = createNewNode(value);
+  Node *currentNode = CreateNewNode(data);
 
-    if(top == NULL)
-    {
-        top = current;
-        end = current;
-    }
-    else
-    {
-        end -> next = current;
-        end = current;
-    }
+  if(top == NULL) 
+  {
+    top = currentNode;
+    end = currentNode;
+  }
+  else
+  {
+    end -> next = currentNode;
+    end = currentNode;
+  }
 }
 
-void delete(int value) // Deletes the given element
+void AddToTop(int data)
 {
-    if(top == NULL){ printf("List is empty"); return; }
+  Node *currentNode = CreateNewNode(data);
 
-    Node *current = top;
-    Node *previous = NULL;
-
-    while(current != NULL)
-    {
-        if(current -> value == value)
-        {
-            if(previous == NULL) // Deleting the head
-            {
-                top = current -> next;
-
-                current = NULL;
-                free(current);
-
-                return;
-            }
-            else // Deleting any other element
-            {
-                previous -> next = current -> next;
-
-                if(previous -> next == NULL){ end = previous; } // If the deleted element was the end, pass 
-                                                                // that reference to the previous element
-                current = NULL;
-                free(current);
-                
-                return;
-            }
-        }
-
-        previous = current;
-        current = current -> next;
-    }
-
-    printf("Element not found\n"); return;
+  if(top == NULL)
+  {
+    top = currentNode;
+    end = currentNode;
+  }
+  else
+  {
+    currentNode -> next = top;
+    top = currentNode;
+  }
 }
 
-
-void printList() // Prints the List
+bool Contains(int data)
 {
-    if(top == NULL){ printf("List is empty"); return; }
+  if(top == NULL) { return false; }
 
-    Node *current = top;
+  Node *currentNode = top;
 
-    printf("[ ");
-    while(current != NULL)
+  while(currentNode != NULL)
+  {
+    if(currentNode -> data == data)
     {
-        printf("%d ", current -> value);
-        current = current -> next;
+      return true;
     }
-    printf("] \n");
+    currentNode = currentNode -> next;
+  }
+
+  return false;
+}
+
+int Pop(int data)
+{
+  if(top == NULL) { return -1; }
+
+  Node *currentNode = top;
+  Node *previousNode = NULL;
+
+  while(currentNode != NULL)
+  {
+    if(currentNode -> data == data)
+    {
+      if(previousNode == NULL)
+      {
+        top = currentNode -> next;
+        return currentNode -> data;
+
+        currentNode = NULL;
+        free(currentNode);
+      }
+      else if(currentNode -> next == NULL)
+      {
+        end = previousNode;
+        previousNode -> next = NULL;
+        return currentNode -> data;
+
+        currentNode = NULL;
+        free(currentNode);        
+      }
+      else
+      {
+        previousNode -> next = currentNode -> next;
+        return currentNode -> data;
+
+        currentNode = NULL;
+        free(currentNode);
+      }
+    }
+
+    previousNode = currentNode;
+    currentNode = currentNode -> next;
+  }
+
+  return -1;
+}
+
+int GetIndex(int data)
+{
+  if(top == NULL) { return -1; }
+
+  Node *currentNode = top;
+  int cont = 0;
+
+  while(currentNode != NULL)
+  {
+    if(currentNode -> data == data)
+    {
+        return cont;
+    }
+
+    cont ++;
+    currentNode = currentNode -> next;
+  }
+
+  return -1;
+}
+
+int GetSize()
+{
+  if(top == NULL) { return 0; }
+  
+  Node *currentNode = top;
+  int cont = 0;
+
+  while(currentNode != NULL)
+  {
+    cont++;
+    currentNode = currentNode -> next;
+  }
+
+  return cont;
+}
+
+void Insert(int index, int data)
+{
+  if(index > GetSize()) { return; }
+
+  Node *nodeToInsert = CreateNewNode(data);
+  Node *currentNode = top;
+  Node *previousNode = NULL;
+  int i = 0;
+
+  while(i <= index)
+  {
+    if(i == index)
+    {
+      if(previousNode == NULL)
+      {
+        nodeToInsert -> next = top;
+        top = nodeToInsert;
+        return;
+      }
+      else
+      {
+        previousNode -> next = nodeToInsert;
+        nodeToInsert -> next = currentNode;
+        return;
+      }
+    }
+
+    i++;
+    previousNode = currentNode;
+    currentNode = currentNode -> next;
+  }
+}
+
+int GetTop()
+{
+  if(top == NULL) { return -1; }
+  
+  return top -> data;
+}
+
+int GetEnd()
+{
+  if(top == NULL) { return -1; }
+  
+  return end -> data;
+}
+
+void PrintList()
+{
+  if(top == NULL) { return; }
+  
+  Node *currentNode = top;
+
+  while(currentNode != NULL)
+  {
+      printf("%d\n", currentNode -> data);
+      currentNode = currentNode -> next;
+  }
 }
 
 int main()
 {
-    for(int i = 0; i < 4; i++)  // Filling list with numbers from 1 to 4
-    {
-        add(i + 1);
-    }
+  AddToEnd(1); AddToEnd(2); AddToEnd(3);
+  AddToTop(-1);
 
-    printf("Initial List: \n");
-
-    printList(); printf("Top is : %d / End is %d\n", top -> value, end -> value);   // Output : [ 1 2 3 4 ]
-                                                                                    // Top is : 1 / End is : 4                                                                            
-    delete(1);
-    delete(2);  // Deleting top, end, and middle element of the list
-    delete(4);
-
-    printf("\nList after delete: \n");
-
-    printList(); printf("Top is : %d / End is %d\n", top -> value, end -> value);   // Output : [ 3 ]
-                                                                                    // Top is : 3 / End is : 3
-    return 0;
+  PrintList();
+  
+  return 0;
 }
